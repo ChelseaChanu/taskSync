7680f12789c8d2a71bebcb87d0236a9937fc4918
Chelsea Chanu <chelseach1asnu@gmail.com>
2025-10-07 10:10:09 +0530

    Dashboard layout fixed for small screen

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,12 +4,12 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="flex w-full min-h-screen overflow-x-hidden bg-[#060C1A]">
+    <div className="flex flex-col md:flex-row w-full min-h-screen overflow-x-hidden bg-[#060C1A]">
       <Sidebar/>
       <div className="flex-1 h-screen overflow-y-auto">
         <Outlet />
       </div>
-    </div>
+    </div> 
   );
 }
 
